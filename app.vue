17f94a48695bae2259fd775251289a5d17cf92b8
czishere <13999553+czishere@user.noreply.gitee.com>
2026-04-11 16:27:16 +0800

    删除不必要配置

--- app.vue
+++ app.vue
@@ -1,16 +1,13 @@
 <template>
-  <n-message-provider>
     <NConfigProvider inline-theme-disabled>
           <NuxtLayout>
             <NuxtPage/>
           </NuxtLayout>
     </NConfigProvider>
-  </n-message-provider>
 </template>
 <script setup>
 import {
   NConfigProvider,
-  NMessageProvider
 } from "naive-ui"
 </script>
 
